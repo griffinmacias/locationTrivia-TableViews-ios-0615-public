b1393be40f72057ad5f8398fd3a09b60351711e0
griffinmacias <griffinmacias@gmail.com>
2015-06-16 15:06:01 -0400

    Configured TableView for locations

---
+++ locationTrivia-tableviews/FISLocationsTableViewController.h
@@ -0,0 +1,20 @@
+//
+//  FISLocationsTableViewController.h
+//  locationTrivia-tableviews
+//
+//  Created by Mason Macias on 6/16/15.
+//  Copyright (c) 2015 Joe Burgess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISLocationsTableViewController : UITableViewController
+
+@property (nonatomic, strong) NSArray *locations;
+
+
+
+
+
+
+@end
---
+++ locationTrivia-tableviews/FISTriviaTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  FISTriviaTableViewController.h
+//  locationTrivia-tableviews
+//
+//  Created by Mason Macias on 6/16/15.
+//  Copyright (c) 2015 Joe Burgess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class FISLocation;
+@interface FISTriviaTableViewController : UITableViewController
+
+
+@property (nonatomic, strong) FISLocation *location;
+
+@end
